5133a2cd8f4ff8f4379880d90dd39c2ce3e0e83e
klykov <lykov.kirill@gmail.com>
2022-05-27 18:18:13 +0200

    add script to install perf

---
+++ net/scripts/install-perf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# perf setup
+#
+set -ex
+
+[[ $(uname) = Linux ]] || exit 1
+[[ $USER = root ]] || exit 1
+
+apt-get --assume-yes install linux-tools-common linux-tools-generic linux-tools-`uname -r`
+
+# setup permissions
+echo -1 | tee /proc/sys/kernel/perf_event_paranoid
+sh -c "echo 0 > /proc/sys/kernel/kptr_restrict"
